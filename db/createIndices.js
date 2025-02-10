5b29866bd0f327d0f2eb4b1d952eaf175c1a9ee3
Martin Svoboda <svobma96@gmail.com>
2025-02-10 19:32:39 +0100

    Add createIndices script

--- db/createIndices.js
+++ db/createIndices.js
@@ -1,13 +1,50 @@
 const client = require('./client');
 
-async function createIndices() {
-  await client.connect();
-  console.log('creating id index');
-  await client.query('DROP INDEX IF EXISTS registrations_id_idx;');
-  await client.query(
-    'CREATE INDEX CONCURRENTLY IF NOT EXISTS registrations_id_idx ON registrations (id);'
+const idIndex = 'registrations_id_idx';
+const znackaIndex = 'registrations_tovarni_znacka_idx';
+const barvaIndex = 'registrations_barva_idx';
+const datumIndex = 'datum_1_registrace';
+
+async function dropIndices() {
+  console.log('dropping indices');
+  await Promise.all(
+    [idIndex, znackaIndex, barvaIndex, datumIndex].map((indexName) =>
+      client
+        .query(`DROP INDEX IF EXISTS ${indexName};`)
+        .then(() => console.log(`dropped index ${indexName}`))
+    )
   );
-  console.log('created id index');
+  console.log('dropped indices');
 }
 
-createIndices();
+async function createIndices() {
+  console.log('creating indices');
+  await client.query(
+    `CREATE INDEX CONCURRENTLY IF NOT EXISTS ${idIndex} ON registrations (id);`
+  );
+
+  console.log('created id index');
+  await client.query(`CREATE INDEX CONCURRENTLY IF NOT EXISTS ${znackaIndex} 
+ON registrations (tovarni_znacka);
+`);
+
+  console.log('created tovarni_znacka index');
+  await client.query(`CREATE INDEX CONCURRENTLY IF NOT EXISTS ${barvaIndex} 
+ON registrations (barva);`);
+  console.log('created barva index');
+
+  await client.query(`CREATE INDEX CONCURRENTLY idx_registrations_year
+ON registrations (datum_1_registrace);`);
+  await client.query(`CREATE INDEX CONCURRENTLY idx_registrations_year_extract
+ON registrations (EXTRACT(YEAR FROM datum_1_registrace));`);
+  console.log('created datum_1_registrace index');
+}
+
+async function run() {
+  await client.connect();
+  await dropIndices();
+  await createIndices();
+  await client.end();
+}
+
+run();
