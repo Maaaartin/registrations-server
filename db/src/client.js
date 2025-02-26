b97eb293f74866a4e65077d1904f5019e900d077
Martin Svoboda <svobma96@gmail.com>
2025-02-26 21:59:50 +0100

    Add infra container

--- db/src/client.js
+++ db/src/client.js
@@ -3,7 +3,7 @@ const { Client } = require('pg');
 
 const client = new Client({
   user: process.env.POSTGRES_USER,
-  host: 'localhost',
+  host: 'postgres',
   database: 'registrations_cz',
   password: process.env.POSTGRES_PASSWORD,
   port: 5432,
