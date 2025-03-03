5f72aff919ac8ec7ffba7310b3dd4c29533c9a4a
Martin Svoboda <svobma96@gmail.com>
2025-03-03 20:30:23 +0100

    Add entrypoint file

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+
+if [ -f .env ]; then
+  export $(grep -v '^#' .env | xargs)
+fi
+
+if [ -f /.dockerenv ]; then
+  HOST="postgres"
+else
+  HOST="localhost"
+fi
+
+export DATABASE_URL="postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$HOST/$POSTGRES_DB?schema=public"
+exec next $1
