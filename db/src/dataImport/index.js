e018a6c92e4aee40959adb56bfe79cb949139fdb
Martin Svoboda <svobma96@gmail.com>
2025-03-08 17:59:16 +0100

    Fix import

--- db/src/dataImport/index.js
+++ db/src/dataImport/index.js
@@ -1,41 +1,73 @@
-const Timers = require('timers/promises');
-const PQueue = require('p-queue').default;
+const fs = require('fs');
 const client = require('../client');
 const {
-  getBatch,
   processRecord,
-  createTableFromHeaders
+  createTableFromHeaders,
+  escapeCSVValue,
+  logError
 } = require('./helpers');
-const { parseLines } = require('./parser');
-const { insertValues } = require('./db');
+const { parse } = require('csv-parse');
+const copyFrom = require('pg-copy-streams').from;
+const schema = require('../schema.json');
+const headerMap = require('../headerMap.json');
+const { PassThrough, pipeline } = require('stream');
 
-async function run() {
-  const queue = new PQueue({ concurrency: 5 });
-  do {
-    const lines = await getBatch();
-    if (!lines.length) {
-      break;
+function run() {
+  const parser = parse({
+    columns: Object.keys(headerMap),
+    relaxColumnCount: true,
+    quote: String.fromCodePoint(0x0022),
+    relaxQuotes: true,
+    record_delimiter: '\r\n',
+    raw: true,
+    fromLine: 1
+  });
+
+  const fileStream = fs.createReadStream(
+    '/Users/martin/Downloads/RSV_vypis_vozidel_20250204.csv'
+  );
+  fileStream.pipe(parser);
+  let lineNr = 0;
+  const interval = setInterval(() => {
+    console.log('line', lineNr);
+  }, 1000);
+
+  const copyStream = client.query(
+    copyFrom(
+      `COPY registrations (${Object.keys(schema).join(', ')}) FROM STDIN WITH CSV`
+    )
+  );
+
+  const passThrough = new PassThrough();
+  parser.on('readable', function () {
+    let data;
+    while ((data = parser.read()) !== null) {
+      lineNr++;
+      const values = processRecord(data.record);
+      passThrough.write(values.map(escapeCSVValue).join(',') + '\n');
     }
-    const records = parseLines(lines);
-    const valueBatch = records.map(processRecord);
-    while (queue.size >= 30) {
-      await Timers.setTimeout(10);
+  });
+
+  const end = (msg) => async (err) => {
+    console.log(msg);
+    if (err) {
+      await logError('./errors_db.csv', err.message);
     }
-    queue.add(() => insertValues(valueBatch));
-  } while (true);
-  await queue.onIdle();
+    process.exit(1);
+  };
+  fileStream.on('error', end('fileStream, error'));
+
+  parser.on('error', end('parser, error'));
+  parser.on('end', () => {
+    passThrough.end();
+  });
+
+  pipeline(passThrough, copyStream, async (err) => {
+    clearInterval(interval);
+    console.log(err);
+    await client.end();
+    console.log('all done');
+  });
 }
 
-module.exports = function () {
-  client
-    .connect()
-    .then(createTableFromHeaders)
-    .then(run)
-    .catch((error) => {
-      console.log('error', error);
-    })
-    .finally(() => {
-      console.log('Finished');
-      return client.end();
-    });
-};
+client.connect().then(createTableFromHeaders).then(run);
