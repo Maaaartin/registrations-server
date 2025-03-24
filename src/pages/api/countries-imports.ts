a8c9a0c511830c57c5f92a158b9b92908bcdf4c2
Martin Svoboda <svobma96@gmail.com>
2025-03-24 18:59:30 +0100

    fix linting

--- src/pages/api/countries-imports.ts
+++ src/pages/api/countries-imports.ts
@@ -1,7 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { countriesImports_ } from '../../../prisma/queries';
 import { ValueCountPairs } from '../../util/data';
-import zod from 'zod';
 
 export default async function handler(
   req: NextApiRequest,
