e493abeb8e1434be3d0f0c8f06694e6a35a3c598
Martin Svoboda <svobma96@gmail.com>
2025-04-04 16:52:04 +0200

    Mouth correct volume

--- run-infra.sh
+++ run-infra.sh
@@ -1,1 +1,1 @@
-docker run --rm --name infra-container --network registrations-server_app_network --env-file .env -v ./data/data.csv:/app/data.csv infra-image "$@"
+docker run --rm --name infra-container --network registrations-server_app_network --env-file .env -v ./data/:/app/data infra-image "$@"
