a10c56612bf7662b7f6fcad7e90b38fbf62c7b4a
Martin Svoboda <svobma96@gmail.com>
2025-04-06 19:33:26 +0200

    Add query timeout

--- prisma/index.ts
+++ prisma/index.ts
@@ -1,3 +1,20 @@
+import Timers from 'timers/promises';
 import { PrismaClient } from './client/default';
-const prisma = new PrismaClient();
+
+const prisma = new PrismaClient().$extends({
+  query: {
+    $allModels: {
+      async $allOperations({ args, query }) {
+        const res = await Promise.race([
+          query(args),
+          Timers.setTimeout(30 * 1000)
+        ]);
+        if (!res) {
+          throw new Error('Query timeout');
+        }
+        return res;
+      }
+    }
+  }
+});
 export default prisma;
