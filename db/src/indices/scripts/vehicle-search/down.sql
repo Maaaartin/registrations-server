5b8276d81efbffd46c5f4004b148326efb496cde
Martin Svoboda <svobma96@gmail.com>
2025-04-07 15:53:28 +0200

    Add indices

--- db/src/indices/scripts/vehicle-search/down.sql
+++ db/src/indices/scripts/vehicle-search/down.sql
@@ -3,3 +3,5 @@ DROP INDEX IF EXISTS idx_vehicle_search_base;
 DROP INDEX IF EXISTS idx_vehicle_search_electric;
 
 DROP INDEX IF EXISTS idx_vehicle_search_hybrid;
+
+DROP INDEX IF EXISTS idx_vehicle_search_id
--- db/src/indices/scripts/vehicle-search/refresh.sql
+++ db/src/indices/scripts/vehicle-search/refresh.sql
@@ -3,3 +3,5 @@ REINDEX INDEX idx_vehicle_search_base;
 REINDEX INDEX idx_vehicle_search_electric;
 
 REINDEX INDEX idx_vehicle_search_hybrid;
+
+REINDEX INDEX idx_vehicle_search_id;
