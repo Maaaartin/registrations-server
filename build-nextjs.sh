dfbd9e0f31e4061377582bd77bd9cb0fb7acb710
Martin Svoboda <svobma96@gmail.com>
2025-04-08 19:04:26 +0200

    Add no cache

--- build-nextjs.sh
+++ build-nextjs.sh
@@ -1,6 +1,6 @@
 sh prepare.sh
 IMAGE_ID="nextjs-image"
-docker build --platform linux/amd64 -t $IMAGE_ID .
+docker build --no-cache --platform linux/amd64 -t $IMAGE_ID .
 sh save-image.sh $IMAGE_ID
 sh copy-image.sh $IMAGE_ID
 rm -rf ./temp
