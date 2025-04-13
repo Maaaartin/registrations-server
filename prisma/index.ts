7257d9f89636a71689b125e542721bd3d2c51cd2
Martin Svoboda <svobma96@gmail.com>
2025-04-13 20:29:40 +0200

    Add transaction to query

--- prisma/index.ts
+++ prisma/index.ts
@@ -1,5 +1,26 @@
 import { PrismaClient } from './client';
 
-const prisma = new PrismaClient();
+const prismaBase = new PrismaClient();
+const prisma = prismaBase.$extends({
+  query: {
+    $allModels: {
+      async $allOperations({ args, query }) {
+        try {
+          return await prismaBase.$transaction(() => query(args), {
+            timeout: 30 * 1000
+          });
+        } catch (error) {
+          if (
+            error instanceof Error &&
+            error.message.match(/Transaction API error/)
+          ) {
+            throw new Error('Query timeout');
+          }
+          throw error;
+        }
+      }
+    }
+  }
+});
 
-export default prisma;
+export default prisma as typeof prismaBase;
