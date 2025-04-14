b3853826f44cbe284d5234beaf1a1ab8109c509b
Martin Svoboda <svobma96@gmail.com>
2025-04-14 16:56:20 +0200

    Add cache

--- src/content/vehicle/server.ts
+++ src/content/vehicle/server.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod';
 import prisma from '../../../prisma';
 import { serialize } from '../data';
+import { unstable_cache } from 'next/cache';
 
 export const queryDecoder = z.object({
   id: z
@@ -18,21 +19,23 @@ export async function getVehicle(id: number) {
 
 const omit = { id: true, pcv: true } as const;
 
-export async function getImportFromPcv(pcv: number | null) {
+export const getImportFromPcv = unstable_cache(async (pcv: number | null) => {
   if (pcv == null) return null;
   const result = await prisma.imports.findFirst({
     where: { pcv },
     omit
   });
   return result ? serialize(result) : null;
-}
+});
 
 export type VehicleImportFromPcv = Exclude<
   Awaited<ReturnType<typeof getImportFromPcv>>,
   null
 >;
 
-export async function getInspectionsFromPcv(pcv: number | null) {
+export const getInspectionsFromPcv = unstable_cache(async function (
+  pcv: number | null
+) {
   if (pcv == null) return [];
   const result = await prisma.inspections.findMany({
     where: { pcv },
@@ -40,27 +43,31 @@ export async function getInspectionsFromPcv(pcv: number | null) {
     orderBy: { platnost_do: 'asc' }
   });
   return result.map(serialize);
-}
+});
 
 export type VehicleInspectionFromPcv = Awaited<
   ReturnType<typeof getInspectionsFromPcv>
 >[0];
 
-export async function getVehicleRemoval(pcv: number | null) {
+export const getVehicleRemoval = unstable_cache(async function (
+  pcv: number | null
+) {
   if (pcv == null) return null;
   const result = await prisma.removed_vehicles.findFirst({
     where: { pcv },
     omit
   });
   return result ? serialize(result) : null;
-}
+});
 
 export type VehicleRemovalFromPcv = Exclude<
   Awaited<ReturnType<typeof getVehicleRemoval>>,
   null
 >;
 
-export async function getVehicleOwnerFromPcv(pcv: number | null) {
+export const getVehicleOwnerFromPcv = unstable_cache(async function (
+  pcv: number | null
+) {
   if (pcv == null) return [];
   const result = await prisma.owners.findMany({
     where: { pcv },
@@ -68,7 +75,7 @@ export async function getVehicleOwnerFromPcv(pcv: number | null) {
     orderBy: { datum_do: 'asc' }
   });
   return result.map(serialize);
-}
+});
 
 export type VehicleOwnerFromPcv = Awaited<
   ReturnType<typeof getVehicleOwnerFromPcv>
