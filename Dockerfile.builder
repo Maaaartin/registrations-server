9a5f9568776bd5f1f3d171edde0f7081dc765d6b
Martin Svoboda <svobma96@gmail.com>
2025-04-17 11:14:29 +0200

    Revert builder image

--- Dockerfile.builder
+++
@@ -1,8 +0,0 @@
-FROM node:18-alpine
-
-WORKDIR /app
-
-COPY package.json yarn.lock ./
-
-RUN yarn --frozen-lockfile 
-
--- nextjs-builder.sh
+++ nextjs-builder.sh
@@ -1,12 +1,13 @@
 sh prepare.sh
 COMMANDS=$(cat <<'EOF'
+yarn --frozen-lockfile
 yarn build
 rm -rf ./.next/cache
 rm -rf ./prisma/client
 tar -cf dist.tar .next prisma public
 EOF
 )
-docker run --rm -it -w /app -v ./temp/:/app/ nextjs-builder sh -c "$COMMANDS"
+docker run --rm -it -w /app -v ./temp/:/app/ node:18-alpine sh -c "$COMMANDS"
 mv ./temp/dist.tar ./dist.tar
 sh copy-file.sh dist.tar
 rm -rf ./temp
