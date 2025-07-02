3f6796b2918c6100be5c56472a8f7a49fe361abc
Martin Svoboda <svobma96@gmail.com>
2025-07-02 21:52:13 +0200

    Upgrade builder node

--- nextjs-builder.sh
+++ nextjs-builder.sh
@@ -7,7 +7,7 @@ rm -rf ./prisma/client
 tar -cf dist.tar .next prisma public
 EOF
 )
-docker run --rm -it -w /app -v ./temp/:/app/ node:18-alpine sh -c "$COMMANDS"
+docker run --rm -it -w /app -v ./temp/:/app/ node:20-alpine sh -c "$COMMANDS"
 mv ./temp/dist.tar ./dist.tar
 sh copy-file.sh dist.tar
 rm -rf ./temp
